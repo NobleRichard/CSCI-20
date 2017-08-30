d19b0b8ecbcb933aa289ca79799fe8ee3cbad3c9
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-08-30 01:53:08 +0000

    Lab12 Commit

---
+++ Lab12/Lab12.cpp
@@ -0,0 +1,29 @@
+/*Created BY:Richard Gutierrez
+        Date: 8/24/16
+        ASCII
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "#############################################################" <<endl;
+	cout << "#           /\\          /\\__/\\          /\\                  #" <<endl;
+	cout << "#          /  \\        |      |        /  \\                 #" <<endl;
+	cout << "#         /    \\       |      |       /    \\                #" <<endl;
+	cout << "#        /      \\_____/        \\_____/      \\               #" <<endl;
+	cout << "#       /                                    \\              #" <<endl;
+	cout << "#      /                                      \\             #" <<endl;
+	cout << "#     |                                       |             #" <<endl;
+	cout << "#     |                                       |             #" <<endl;
+	cout << "#     \\     ___  _               _  ___      /              #" <<endl;
+	cout << "#      \\   |   \\/ \\             / \\/   |    /               #" <<endl;
+	cout << "#       \\  |       \\           /       |   /                #" <<endl;
+	cout << "#        \\ |        \\         /        |  /                 #" <<endl;
+	cout << "#         \\|         \\       /         | /                  #" <<endl;
+	cout << "#                     \\     /                               #" <<endl;
+    cout << "#                      \\   /                                #" <<endl;
+	cout << "#                       \\ /                                 #" <<endl;
+	cout << "#############################################################" <<endl;
+return 0;
+}
