01becad9c0098dd207a769d124c1e5981f33b08e
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-09-01 02:08:52 +0000

    Lab1.3 submission

--- Lab13/Lab13.cpp
+++ Lab13/Lab13.cpp
@@ -4,8 +4,16 @@ using namespace std;
     int years;
     int future;
     
+    int sec = 60;
+    int minu = 60;
+    int hour = 24;
+    int days = 365;
+    
+    double per = 12;
     int pop = 325765736; //population 
-    int net = 2628000; //people gained per year
+    int net = 1; //number of people added every second p/s
+    double netgain ; // net gain per year
+    
     int gain; // population added with population over years
 
 int main() {
@@ -13,9 +21,13 @@ int main() {
     cout << "The population of the United States on August 30th, 2017 was 325,765,736 people." << endl;
     cout << "Type in how many years into the future you would like to travel, so that you may view the population number there." << endl;
     cin >> years;
-    gain = pop + (years*net);
+    
+    netgain = (net / per * sec * minu * hour * days); 
+    gain = pop + (years * netgain);
+    
     future = 2017 + years;
-    cout << "There will be approximately " << gain << " people alive in the UNited States in the year " << future << "." << endl;
+    
+    cout << "There will be approximately, " << gain << " people alive in the United States in the year " << future << "." << endl;
     
     
     
