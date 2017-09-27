26707af2afb15cdcf2ebdad201212c072cb6de79
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-09-27 02:17:31 +0000

    Lab2.3 submission

--- Lab23/Lab23.cpp
+++ Lab23/Lab23.cpp
@@ -15,13 +15,13 @@ void Switcheroo(int& little, int& big){
     return;
 }
 
-void Rand(int small, int huge){
+void Rand(int small, int huge){  //gives random number 
+     
     
-    
-    Switcheroo( small, huge ) ;
+    Switcheroo( small, huge ) ;  //call swap from within a function
     srand(time(0));
     int x = 0;
-    x = rand () % (huge-small) + small + 1;
+    x = rand () % (huge-small) + small + 1; //this way it makes the random number between the two inputed numbers
     cout << "Your random number between " << small << " and " << huge << " is: " << x << endl;
 }
 
@@ -37,7 +37,7 @@ int main(){
     cin >> Num2;
     
 
-    Rand(Num1, Num2);
+    Rand(Num1, Num2); //call function for random number
     
     return 0;
 }
@@ -45,6 +45,6 @@ int main(){
 
 /*
 Your random number between 1 and 10 is: 7
-
-
+Your random number between 5 and 82 is: 61
+Your random number between 22 and 73 is: 38
 */
