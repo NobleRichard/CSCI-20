1dd227690e71e3c53ee2bfb29b14cbcd9eb83093
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-10-18 01:43:35 +0000

    Lab3.3 submission

--- Lab33/Lab33.cpp
+++ Lab33/Lab33.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-int Rand(){
+int Rand(){  //random number generator function
     srand(time(0));
     int x = 0;
     x = rand () % 50;
@@ -16,22 +16,22 @@ int main(){
     int c = 0;
     cout << "Guess my number. Input a number 1 - 50." << endl;
     cin >> p;
-    while  (p > 50 || p < 1){
+    while  (p > 50 || p < 1){ // checks to make sure user input fits criteria
         cout << "Please input a number between 1 and 50." << endl;
         cin >> p;
     }
-    c = Rand();
+    c = Rand(); //assign random number to c so that we can use it
     
-    while (c != p){
-    if(p > c){
-        for (int i = 0; i < 5; i++ ){
+    while (c != p){ //making sure that it only runs when both user number and random number are not equal
+    if(p > c){ // to check is number is too big
+        for (int i = 0; i < 5; i++ ){ //used to print out 5..4..3..2..1 one at a time to make it more dramatic
         cout << 5 - i << "..." << endl;
         }
         cout << "Your number is too big!" << endl;
         cout << "Try again!" << endl;
         cin >> p;
     }
-    else {
+    else { //check if number is too small
         for (int i = 0; i < 5; i++ ){
         cout << 5 - i << "..." << endl;
         }
