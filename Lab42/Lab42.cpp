5ac0e02dc1388df2124d6285bff52187c6a395d1
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-11-10 20:36:03 +0000

    Lab44 submission

--- Lab42/Lab42.cpp
+++ Lab42/Lab42.cpp
@@ -19,23 +19,21 @@ using namespace std;
 int main(){
  srand(time(0));
  string storageName[10] = {"chicken", "juice", "milk", "rice", "peppers", "chips", "eggs", "oranges", "cheese", "candy"};
- int Storagenum[10];
+ int Storagenum[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
  int StorageStock[10];
  float StoragePrice[10] = {5.67, 2.36, 3.25, 3.75, .58, 2.00, 4.56, .90, 4.37, 1.25};
  
- int ShopperItems[10];
+ int ShopperItems[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
  int ShopperAmounts[10];
+ int x = 0;
  
  
- 
- for (int j = 0; j < 10; j++){
-            Storagenum[j] = j + 1;
- }
-for(int i = 0; i < 10; i++){
-    StorageStock[i] = rand() % 10 + 1;
+
+for(int i = 0; i < 10; i++){//assigns amount of inventory
+    StorageStock[i] = rand() % 10 + 1; 
 }
  cout << "This is what we have in stock, what its item number is, how many of the item we have in stock, and how much it costs." << endl;
- for (int j = 0; j < 10; j++){
+ for (int j = 0; j < 10; j++){ //prints out the inventory
     cout << storageName[j] << "      " << Storagenum[j] << "       " << StorageStock[j] << "    $" << StoragePrice[j] << endl;
  }
 
@@ -49,10 +47,19 @@ while(decision == 'y'){
     
     for (int i = 0; i < 10; i++){
         if(storageName[i] == choice) {
-            cout << "You want to buy " << storageName[i] << ". I'll add one to the cart." << endl;
-            
+            cout << "You want to buy " << storageName[i] << ". How many would you like to add to cart.? << endl;
+            cin >> x;
+            if(StorageStock[i] >= x){
+                ShopperAmounts[i] = x; //add to the shopping cart
+                StorageStock[i] = StorageStock[i] - x; //subtract from the stock inventory
+            }
+            else{
+                cout << "We don't have that many items in stock. Take an amount we have please." << endl;
+                cin >> x;
+                ShopperAmounts[i] = x; //add to the shopping cart
+                StorageStock[i] = StorageStock[i] - x; //subtract from the stock inventory
+            }
         }
-        
     }
     
 }
