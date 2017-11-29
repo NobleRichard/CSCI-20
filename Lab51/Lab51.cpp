a710348ca684345057655c9f7adb68d923d65cef
Richard Gutierrez <gutierrez.richard184549@gmail.com>
2017-11-29 03:03:46 +0000

    Lab51 submission

--- Lab51/Lab51.cpp
+++ Lab51/Lab51.cpp
@@ -1,27 +1,28 @@
 #include <iostream> 
-#include <iostream> 
+//#include <iostream> //don't need two iostream libraries
 #include <cstring> 
 using namespace std;   
 
 int main() {
     int nameLength = 0;
-    char * name = new char[];
+    char * name = new char[10]; //creating a new pointer array that can be accessed
 
-    cout << "Please enter a 10 letter word or less" << endl;
-    cin >> name;
-
-    char * head  = name;
-    char * tail = name;
-    nameLength = strlen(*name);
+    cout << "Please enter a 10 letter word or less" << endl; 
+    cin >> name; 
+    cout << name;
+/*
+    char * head  = name; //the pointer head now holds the value given to the variable name
+    char * tail = name;  //the pointer tail also holds the value that was given to name
+    nameLength = strlen(*name);  //gives you the length of the "string" name, 
 
     cout << "Your word is " << firststr << endl;
 
-    if (nameLength<10)
+    if (nameLength<10) 
     {
-         while (*head != '\0')
+         while (*head != '\0') //checking to see if the if statement is true. That the value of head is not null so that it can go through the entire word
          {
-                cout << *head;
-                head++;
+                cout << *head;   //prints out the value in head
+                head++;          
          }
      }
      else
@@ -31,25 +32,25 @@ int main() {
 
      cout << endl;
 
-     tail = &name[strlen(name) - 1];
+     tail = &name[strlen(name) - 1]; // tails is given the value of the address of name[] and inside the brackets go the second to last thing in character array
 
      if (nameLength < 10)
      {
           while (tail != name)
           {
-                 cout << *tail;
+                 cout << *tail; //outputing the value of tail which is now the last letter of the char string 
                  tail--;
           }
      }
      cout << endl;
 
      head = name;
-     tail = &name[strlen(name) - 1];
+     tail = &name[strlen(name) - 1]; //again sets the tail as the adress of name 
 
      head++;
      tail--;
 
-     if (*head == *tail)
+     if (*head == *tail) //checks the 
      {
          cout << "It is an palindrome!" << endl;
      }
@@ -57,6 +58,6 @@ int main() {
      {
          cout << "It is not an palindrome" << endl;
      }
-
+*/
      return 0; 
 }
